19b224b4aa79bbb5f4de5832f7a655b315fc34a6
Minji Song <hesseminji@sookmyung.ac.kr>
2023-06-30 21:44:21 +0900

    commit

--- app/src/main/java/com/madcamp/week1/MainActivity.kt
+++ app/src/main/java/com/madcamp/week1/MainActivity.kt
@@ -15,14 +15,14 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
-    var addList = arrayListOf<Contracts>(
-        Contracts("Song", "https://github.com/minjisong128", "email_minji", "https://avatars.githubusercontent.com/u/110910757?v=4"),
-        Contracts("Hwang", "https://github.com/JeukHwang", "email_jeuk", "https://avatars.githubusercontent.com/u/92910273?v=4"),
-        Contracts("Kim", "https://github.com/kim", "email_kim", "https://avatars.githubusercontent.com/u/6163?v=4"),
-        Contracts("Lee", "https://github.com/lee", "email_lee", "https://avatars.githubusercontent.com/u/2014?v=4"),
-        Contracts("Park", "https://github.com/park", "email_park", "https://avatars.githubusercontent.com/u/135650?v=4"),
-        Contracts("Choi", "https://github.com/choi", "email_choi", "https://avatars.githubusercontent.com/u/10793473?v=4"),
-        Contracts("Lim", "https://github.com/lim", "email_lim", "https://avatars.githubusercontent.com/u/7571747?v=4")
+    var addList = arrayListOf<Info>(
+        Info("Song", "https://github.com/minjisong128", "email_minji", "https://avatars.githubusercontent.com/u/110910757?v=4"),
+        Info("Hwang", "https://github.com/JeukHwang", "email_jeuk", "https://avatars.githubusercontent.com/u/92910273?v=4"),
+        Info("Kim", "https://github.com/kim", "email_kim", "https://avatars.githubusercontent.com/u/6163?v=4"),
+        Info("Lee", "https://github.com/lee", "email_lee", "https://avatars.githubusercontent.com/u/2014?v=4"),
+        Info("Park", "https://github.com/park", "email_park", "https://avatars.githubusercontent.com/u/135650?v=4"),
+        Info("Choi", "https://github.com/choi", "email_choi", "https://avatars.githubusercontent.com/u/10793473?v=4"),
+        Info("Lim", "https://github.com/lim", "email_lim", "https://avatars.githubusercontent.com/u/7571747?v=4")
     )
 
     private lateinit var binding: ActivityMainBinding
