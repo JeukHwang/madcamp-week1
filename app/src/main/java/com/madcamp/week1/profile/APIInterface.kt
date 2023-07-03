fe69cd630eb9ecbd57a27be59ed96e021bafa165
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-07-03 10:40:28 +0900

    feat: integrate already made profile tab

---
+++ app/src/main/java/com/madcamp/week1/profile/APIInterface.kt
@@ -0,0 +1,9 @@
+package com.madcamp.week1.profile
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+interface APIInterface {
+  @GET fun getGithubUser(@Url url: String): Call<GithubUserData>
+}
---
+++ app/src/main/java/com/madcamp/week1/profile/ApiObject.kt
@@ -0,0 +1,14 @@
+package com.madcamp.week1.profile
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiObject {
+  private const val baseUrl = "https://api.github.com/"
+
+  private val getRetrofit by lazy {
+    Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create()).build()
+  }
+
+  val getRetrofitService: APIInterface by lazy { getRetrofit.create(APIInterface::class.java) }
+}
