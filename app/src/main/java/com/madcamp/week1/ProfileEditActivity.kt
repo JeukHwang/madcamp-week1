83bee005a1d7a92148e1100b5145fd4ca51ce554
Minji Song <hesseminji@sookmyung.ac.kr>
2023-07-04 22:12:56 +0900

    feat: able to share profile in text form by clicking button in Develop tab

--- app/src/main/java/com/madcamp/week1/ProfileEditActivity.kt
+++ app/src/main/java/com/madcamp/week1/ProfileEditActivity.kt
@@ -12,6 +12,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class ProfileEditActivity : AppCompatActivity() {
   private lateinit var binding: ActivityProfileEditBinding
 
