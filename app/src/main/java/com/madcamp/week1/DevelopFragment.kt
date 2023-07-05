d1abb56a644353857a6134cafee4622d699549df
Minji Song <hesseminji@sookmyung.ac.kr>
2023-07-05 12:31:44 +0900

    feat: share option -- can share in text form

--- app/src/main/java/com/madcamp/week1/DevelopFragment.kt
+++ app/src/main/java/com/madcamp/week1/DevelopFragment.kt
@@ -1,7 +1,6 @@
 package com.madcamp.week1
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -32,13 +31,7 @@ class DevelopFragment : Fragment() {
         }
 
         R.id.share -> {
-          val intent = Intent(this.context, ProfileShareActivity::class.java)
-          //                    val bundle = Bundle()
-          //                    bundle.putString("info_photo", info.photo)
-          //                    bundle.putString("info_id", info.id)
-          //                    bundle.putString("info_email", info.email)
-          //                    intent.putExtra("bundle_key", bundle)
-          startActivity(intent)
+          shareText()
           true
         }
 
@@ -48,6 +41,7 @@ class DevelopFragment : Fragment() {
     return binding.root
   }
 
+  /*
   private fun shareInstagram() {
     val instagram_urlintent = Intent(Intent.ACTION_VIEW, Uri.parse("https://m.instagram.com/"))
     startActivity(instagram_urlintent)
@@ -57,11 +51,12 @@ class DevelopFragment : Fragment() {
     val github_urlintent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/"))
     startActivity(github_urlintent)
   }
+  */
 
   private fun shareText() {
     val sendIntent: Intent = Intent().apply {
       action = Intent.ACTION_SEND
-      putExtra(Intent.EXTRA_TEXT, "@@@ 앱을 통해 몰입캠프 참가 학생과 프로필을 공유해 보아요.")
+      putExtra(Intent.EXTRA_TEXT, "@@@ 앱에서 함께 프로필을 공유해 보아요.")
       type = "text/plain"
     }
 
