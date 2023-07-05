d52250e2ed745c6f43984ea50221a622dc9fbb60
Minji Song <hesseminji@sookmyung.ac.kr>
2023-07-05 16:41:21 +0900

    feat: can move to another website using Github/Instagram/LinkedIn id in Profile tab

--- app/src/main/java/com/madcamp/week1/DevelopFragment.kt
+++ app/src/main/java/com/madcamp/week1/DevelopFragment.kt
@@ -3,6 +3,11 @@ package com.madcamp.week1
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.TextUtils
+import android.text.method.LinkMovementMethod
+import android.text.util.Linkify
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,6 +23,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class DevelopFragment : Fragment() {
 
   private lateinit var binding: FragmentDevelopBinding
@@ -31,10 +37,14 @@ class DevelopFragment : Fragment() {
     binding = FragmentDevelopBinding.inflate(inflater, container, false)
     binding.topAppBar.setOnMenuItemClickListener { menuItem ->
       when (menuItem.itemId) {
+//        R.id.textField_github_id -> {
+//
+//        }
         R.id.refresh -> {
           fetchUser()
           true
         }
+
         R.id.edit -> {
           //          val resultLauncher =
           //
@@ -110,8 +120,35 @@ class DevelopFragment : Fragment() {
     binding.textFieldClass.editText?.setText("${userProfile.classNum}분반")
     binding.textFieldExplanation.editText?.setText(userProfile.explanation)
     binding.textFieldEmail.editText?.setText(userProfile.email)
-    binding.textFieldGithubId.editText?.setText(userProfile.githubId)
-    binding.textFieldInstagramId.editText?.setText(userProfile.instagramId)
-    binding.textFieldLinkedinId.editText?.setText(userProfile.linkedInId)
+    //binding.textFieldGithubId.editText?.setText(userProfile.githubId)
+    //binding.textFieldInstagramId.editText?.setText(userProfile.instagramId)
+    //binding.textFieldLinkedinId.editText?.setText(userProfile.linkedInId)
+
+    // Make links in the EditText clickable
+    binding.textFieldGithubId.editText?.movementMethod = LinkMovementMethod.getInstance()
+    binding.textFieldInstagramId.editText?.movementMethod = LinkMovementMethod.getInstance()
+    binding.textFieldLinkedinId.editText?.movementMethod = LinkMovementMethod.getInstance()
+
+    // Setup my Spannable with clickable URLs
+    val spannable_github: Spannable = SpannableString("https://github.com/" + userProfile.githubId)
+    Linkify.addLinks(spannable_github, Linkify.WEB_URLS)
+
+    val spannable_instagram: Spannable =
+      SpannableString("https://m.instagram.com/" + userProfile.instagramId)
+    Linkify.addLinks(spannable_instagram, Linkify.WEB_URLS)
+
+    val spannable_linkedin: Spannable =
+      SpannableString("https://m.linkedin.com/" + userProfile.linkedInId)
+    Linkify.addLinks(spannable_linkedin, Linkify.WEB_URLS)
+
+    // The fix: Append a zero-width space to the Spannable
+    val text_github = TextUtils.concat(spannable_github, "\u200B")
+    val text_instagram = TextUtils.concat(spannable_instagram, "\u200B")
+    val text_linkedin = TextUtils.concat(spannable_linkedin, "\u200B")
+
+    // Use it!
+    binding.textFieldGithubId.editText?.setText(text_github)
+    binding.textFieldInstagramId.editText?.setText(text_instagram)
+    binding.textFieldLinkedinId.editText?.setText(text_linkedin)
   }
 }
